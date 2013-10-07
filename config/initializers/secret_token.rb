83b8af66419d3b0a45456210800d95af5290177d
root <root@europa.(none)>
2013-10-07 13:15:50 -0400

    Configured QX

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsrumble::Application.config.secret_key_base = '9662c689a591a22ee521b6037de51558c09441cd64f959034b568c18105e507d282b64a5df402f8267e2497cf5e13ec7f1bf70e884f6da4acd19270dd7c4c742'
